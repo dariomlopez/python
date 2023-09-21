886051e399de2a1b61d8942e359826787bbd8549
dariomlopez <marotodario@gmail.com>
2023-09-21 16:29:26 +0200

    Comentando ejercicio 3, estructuras de datos

--- Estructuras de datos/EJ3.py
+++ Estructuras de datos/EJ3.py
@@ -6,19 +6,19 @@
 #Especio por usuario: 2KB
 # Programa:
 
-import collections
-
 class ListaReproduccion:
   def __init__(self):
     self.ListaCanciones = []
 
   def AgregarCancion(self, cancion = ""):
-    # Para variar un poco sobre el ejercicio de tareas,
     # la canción se añade al principio de la lista con el método insert que acepta dos parametros (index de array, cosa a añadir)
+    # Argumentos que toma la función: esperamos sea tipo string, de momento no se controla
     self.ListaCanciones.insert(0, cancion)
 
   def EliminarCancion(self, cancion = "", index = None):
     # Se puede eliminar una cación por nombre o por index de lista
+    # Argumento necesario: cancion(string)
+    # Argumento no obligatorio: index (int) 
     if cancion in self.ListaCanciones:
       self.ListaCanciones.remove(cancion)
     elif index is not None and index < len(self.ListaCanciones) and index >= 0:
@@ -34,7 +34,8 @@ class ListaReproduccion:
     # Manda la última canción de la lista al primer lugar, pero no módifica la lista
     return self.ListaCanciones.insert(0, self.ListaCanciones.pop())
 
-  def ReproducirLista(self):
+  def reproducir_lista(self):
+    # return el array actual de canciones
     return self.ListaCanciones
   
 lista_reproduccion = ListaReproduccion()

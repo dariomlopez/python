7926e8710a0d4c23ab70f0d1545b24526e5bd08f
dariomlopez <marotodario@gmail.com>
2023-09-25 16:04:56 +0200

    Función buscar producto Ejercicio 6

--- Estructuras de datos/EJ6.py
+++ Estructuras de datos/EJ6.py
@@ -7,8 +7,7 @@ class InventarioProductos:
     self.inventario = []
 
   def agregar_producto(self, nombre, cantidad):
-    self.inventario.append({"nombre": nombre,
-                            "cantidad": cantidad})
+    self.inventario.append({"nombre": nombre, "cantidad": cantidad})
     
   def actualizar_cantidad(self, nombre, nueva_cantidad):
     for producto in self.inventario:
@@ -16,3 +15,36 @@ class InventarioProductos:
         producto["cantidad"] = nueva_cantidad
         return True
     return False
+  
+  def buscar_producto(self, nombre):
+    for producto in self.inventario:
+      if producto["nombre"] == nombre:
+        return producto
+      
+inventario = InventarioProductos()
+
+# Función agregar producto:
+inventario.agregar_producto("Manzanas", 10)
+inventario.agregar_producto("Plátanos", 15)
+inventario.agregar_producto("Aguacates", 12)
+inventario.agregar_producto("Camisetas", 25)
+inventario.agregar_producto("Peras", 22)
+inventario.agregar_producto("Cucharas", 19)
+inventario.agregar_producto("Papel", 4)
+inventario.agregar_producto("Garbanzos", 5)
+
+
+# Función buscar producto:
+print(inventario.buscar_producto("Aguacates"))
+print(inventario.buscar_producto("Papel"))
+print(inventario.buscar_producto("Manzanas"))
+
+# Función actualizar: 
+inventario.actualizar_cantidad("Cucharas", 33)
+inventario.actualizar_cantidad("Garbanzos", 21)
+inventario.actualizar_cantidad("Peras", 13)
+
+#Productos actualizados:
+print(inventario.buscar_producto("Cucharas"))
+print(inventario.buscar_producto("Garbanzos"))
+print(inventario.buscar_producto("Peras"))

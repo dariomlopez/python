20c7e0d771ea9ffa1a0cc810525e7aadccfbdaef
dariomlopez <marotodario@gmail.com>
2023-10-02 18:33:58 +0200

    Primeros pasos con flask

---
+++ Flask/venv/requests_endpoints.py
@@ -0,0 +1,10 @@
+# Endpoints, GET y Post con flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '¡Hola, mundo!'
+
+if __name__ == '__main__':
+    app.run(debug=True)

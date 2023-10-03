e0d1930511fe6c659c2ed97d76a8b7bf28d88767
dariomlopez <marotodario@gmail.com>
2023-10-03 17:15:50 +0200

    Basics of flask

--- Flask/first_flask.py
+++ Flask/first_flask.py
@@ -1,10 +1,15 @@
 # Endpoints, GET y Post con flask
+# Line 2 to line 9 a very basic flask app
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '¡Hola, mundo!'
+def index():
+    return '¡Landing page!'
+
+@app.route('/usuario/<username>')
+def usuario(username):
+    return f'Hola {username}, bienvenida/o a tu perfil'
 
 if __name__ == '__main__':
     app.run(debug=True)

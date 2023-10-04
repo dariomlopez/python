af38ca78b060183c33651da3bb23610198cd95b9
dariomlopez <marotodario@gmail.com>
2023-10-04 16:26:48 +0200

    Learning flask

---
+++ Flask/MyNotes Web App/app.py
@@ -0,0 +1,12 @@
+#04/10/2023 Siguiendo este video-->https://www.youtube.com/watch?v=mqhxxeeTbu0
+#https://www.youtube.com/watch?v=dam0GPOAvVI
+#https://www.youtube.com/watch?v=yBDHkveJUf4   web development with python and flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+if __name__ == "__main__":
+  app.run()
